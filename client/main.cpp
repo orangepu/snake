022706bb24af97365f1fbc7720753bd967f28adb
junchengpu <1332839428@mail.dlut.edu.cn>
2020-03-11 01:12:35 +0800

    Create main.cpp

---
+++ client/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "snake.h"
+#include <QApplication>
+#include <QPixmap>
+#include <QWidget>
+#include <QSplashScreen>
+
+int main(int argc,char *argv[])
+{
+    QApplication a(argc,argv);
+    QPixmap pixmap("logo.ico");
+    QSplashScreen spalsh(pixmap);
+
+    spalsh.show();//启动此显示图片
+    a.processEvents();//是程序在显示启动画面的同时响应鼠标等其他事件
+
+    Snake w;
+    w.show();
+
+    spalsh.finish(&w);
+    return a.exec();
+
+}
